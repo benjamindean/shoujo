856d397929e6c9313b846c45dda2698d3fccef51
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-10-24 18:00:21 +0300

    Reformat

--- browser.js
+++ browser.js
@@ -4,8 +4,8 @@ const elementReady = require('element-ready');
 const ipcRenderer = electron.ipcRenderer;
 const $ = document.querySelectorAll.bind(document);
 
-elementReady('html').then(function() {
-    $('.thumbnails')[0].addEventListener('click', function(e) {
+elementReady('html').then(function () {
+    $('.thumbnails')[0].addEventListener('click', function (e) {
         e.preventDefault();
         console.log(e.target);
     });
--- main.js
+++ main.js
@@ -1,60 +1,60 @@
 const electron = require('electron');
 const app = electron.app;
-const path = require('path')
-const ipcMain = electron.ipcMain
+const path = require('path');
+const ipcMain = electron.ipcMain;
 const BrowserWindow = electron.BrowserWindow;
 require('electron-debug')();
 
 var mainWindow = null;
 
 const instanceRunning = app.makeSingleInstance(() => {
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) {
-      mainWindow.restore();
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+            mainWindow.restore();
+        }
     }
-  }
 });
 
 if (instanceRunning) {
-  app.quit();
+    app.quit();
 }
 
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     app.quit();
 });
 
-app.on('ready', function() {
-  var subpy = require('child_process').spawn('python', ['./shoujo/server.py']);
-  var rq = require('request-promise');
-  var mainAddr = 'http://localhost:5000';
+app.on('ready', function () {
+    var subpy = require('child_process').spawn('python', ['./shoujo/server.py']);
+    var rq = require('request-promise');
+    var mainAddr = 'http://localhost:5000';
 
-  var openWindow = function(){
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          preload: path.join(__dirname, 'browser.js'),
-          nodeIntegration: false
-        }
-    });
-    mainWindow.loadURL('http://localhost:5000');
-    mainWindow.webContents.openDevTools();
+    var openWindow = function () {
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+                preload: path.join(__dirname, 'browser.js'),
+                nodeIntegration: false
+            }
+        });
+        mainWindow.loadURL('http://localhost:5000');
+        mainWindow.webContents.openDevTools();
 
-    mainWindow.on('closed', function() {
-      mainWindow = null;
-      subpy.kill('SIGINT');
-    });
-  };
+        mainWindow.on('closed', function () {
+            mainWindow = null;
+            subpy.kill('SIGINT');
+        });
+    };
 
-  var startUp = function(){
-    rq(mainAddr)
-      .then(function(htmlString){
-        openWindow();
-      })
-      .catch(function(err){
-        startUp();
-      });
-  };
+    var startUp = function () {
+        rq(mainAddr)
+            .then(function (htmlString) {
+                openWindow();
+            })
+            .catch(function (err) {
+                startUp();
+            });
+    };
 
-  startUp();
+    startUp();
 });
