4a33a2f3d99b4e3e880b4abb7f0b5ab1439c684f
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-10-25 14:00:02 +0300

    Temporary way of diplaying images

--- browser.js
+++ browser.js
@@ -4,9 +4,35 @@ const elementReady = require('element-ready');
 const ipcRenderer = electron.ipcRenderer;
 const $ = document.querySelectorAll.bind(document);
 
-elementReady('html').then(function () {
+var loadXMLDoc = function(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+               callback(xmlhttp.responseText);
+           }
+           else {
+               console.log(xmlhttp.status);
+           }
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+};
+
+var listenThumbnails = function () {
     $('.thumbnails')[0].addEventListener('click', function (e) {
         e.preventDefault();
-        console.log(e.target.getAttribute('id'));
+        let id = e.target.getAttribute('id');
+        if(!id) return;
+        loadXMLDoc('http://localhost:5000/image/' + encodeURI(id), function(response) {
+            $('#mainImage')[0].setAttribute('src', 'file://' + response);
+        });
     });
+};
+
+elementReady('html').then(function () {
+    listenThumbnails();
 });
