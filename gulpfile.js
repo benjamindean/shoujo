30b1462d283e23e2ab5ffbb83ed609749bf1b7b4
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-11-14 13:41:34 +0200

    Modify project structure a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass')
 
 gulp.task('css', function () {
-    gulp.src('resources/scss/*.scss')
+    gulp.src('src/scss/*.scss')
         .pipe(sass())
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
@@ -12,13 +12,13 @@ gulp.task('css', function () {
 });
 
 gulp.task('components', function () {
-    gulp.src('resources/components/normalize-css/normalize.css')
+    gulp.src('src/components/normalize-css/normalize.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('shoujo/public/css/'));
 });
 
 gulp.task('default', ['css', 'components'], function () {
-    gulp.watch('resources/scss/*.scss', ['css']);
-    gulp.watch('resources/scss/partials/*.scss', ['css']);
+    gulp.watch('src/scss/*.scss', ['css']);
+    gulp.watch('src/scss/partials/*.scss', ['css']);
 });
